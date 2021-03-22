5d4a4eb5c5637fcc62e564405408fceaa531c866
OctavioUTNFRA <o.villegas@sistemas-utnfra.com.ar>
2021-03-22 19:55:34 -0300

    vacio commit

--- app/index.php
+++ app/index.php
@@ -1,20 +1,2 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-require_once '../vendor/autoload.php';
-
-
-
-$app = new \Slim\App([]);
-
-
-
-$app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("GET => Bienvenido a PHP!!! ,a SlimFramework");
-    return $response;
-
-});
-
-
-$app->run();
+echo "hola";
