0127e70374da152803d7d6904a8f9b575ccebd79
OctavioUTNFRA <o.villegas@sistemas-utnfra.com.ar>
2021-03-23 11:50:25 -0300

    slim4 5

--- app/index.php
+++ app/index.php
@@ -12,6 +12,7 @@ $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 //$app->setBasePath("/slim4/public");
 //$app->setBasePath("/public/index.php");
+$app->setBasePath("https://dashboard.heroku.com/index.php");
 $app->get('/hola/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Saludar!");
     return $response;
