60addaf85fad010bdb4c59d1e09d2626e5a907a2
OctavioUTNFRA <o.villegas@sistemas-utnfra.com.ar>
2021-03-23 12:03:51 -0300

    slim4 8

--- app/index.php
+++ app/index.php
@@ -11,8 +11,8 @@ $app->addRoutingMiddleware();
 
 $app->addErrorMiddleware(true, true, true);
 //$app->setBasePath("/slim4/public");
-//$app->setBasePath("/public/index.php");
-
+//$app->setBasePath("/slim4/public/index.php");
+$app->setBasePath("/app/index.php");
 $app->get('/hola/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Saludar!");
     return $response;
